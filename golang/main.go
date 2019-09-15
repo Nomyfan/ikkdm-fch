5754036ba9e68cef88793c6c199281c6f09d4be1
nomyfan <Nomyfan@live.com>
2019-09-15 13:13:43 +0800

    update

--- golang/main.go
+++ golang/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	_ "github.com/PuerkitoBio/goquery"
-	"github.com/axgle/mahonia"
-	"github.com/gocolly/colly"
+	"github.com/PuerkitoBio/goquery"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -14,7 +12,17 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+
+	_ "github.com/PuerkitoBio/goquery"
+	"github.com/axgle/mahonia"
+	"github.com/gocolly/colly"
 )
+
+// Episode 每一话的标题和链接
+type Episode struct {
+	title string
+	href  string
+}
 
 func saveImg(link string, title string, name string, ch chan bool) {
 	if res, err := http.Get(link); err == nil {
@@ -46,60 +54,63 @@ func fchImg(title string, url string, ch chan bool) {
 	_ = cly.Visit(url)
 }
 
-func fchEachEpisode(urlPath string, channel chan bool) {
-	decoder := mahonia.NewDecoder("gbk")
+func fchEachEpisode(episode Episode, channel chan bool) {
 	host := "http://m.ikkdm.com"
-	link := host + urlPath
+	link := host + episode.href
 	cly := colly.NewCollector()
 	cly.OnHTML("div.classBox.autoHeight", func(element *colly.HTMLElement) {
 
-		txtA := decoder.ConvertString(element.DOM.Find("ul > center > li[class=txtA]").Text())
-		split := strings.Split(txtA, "|")
-		// this first is title
-		title := strings.TrimSpace(split[0])
-		// the second is episodeCount
-		episodeCount, _ := strconv.ParseInt(strings.TrimSpace(strings.Split(split[1], "/")[1]), 10, 64)
-		fmt.Printf("标题: %s, 页数: %d \n", title, episodeCount)
+		lis := element.DOM.Find("div.bottom ul.subNav li")
+		var info string
+		lis.Each(func(i int, selection *goquery.Selection) {
+			if 1 == i {
+				info = selection.Text()
+			}
+		})
+		split := strings.Split(info, "/")
+		episodeCount, _ := strconv.ParseInt(strings.TrimSpace(split[1]), 10, 64)
 
-		if _, err := os.Stat("download/" + title); os.IsNotExist(err) {
-			if err := os.Mkdir("download/"+title, 0777); err != nil {
-				fmt.Println("无法为 {{ " + title + " }} 创建目录")
+		if _, err := os.Stat("download/" + episode.title); os.IsNotExist(err) {
+			if err := os.Mkdir("download/"+episode.title, 0777); err != nil {
+				fmt.Println("无法为 {{ " + episode.title + " }} 创建目录")
 				return
 			}
 		}
 
-		baseUrl := link[0 : strings.LastIndex(link, "/")+1]
+		baseURL := link[0 : strings.LastIndex(link, "/")+1]
 		ch := make(chan bool, episodeCount)
 		for i := int64(1); i <= episodeCount; i++ {
-			url := baseUrl + strconv.FormatInt(i, 10) + ".htm"
-			go fchImg(title, url, ch)
+			url := baseURL + strconv.FormatInt(i, 10) + ".htm"
+			go fchImg(episode.title, url, ch)
 		}
 		// 等待当前话的所有图片下载完成
 		for i := int64(1); i <= episodeCount; i++ {
 			<-ch
 		}
 		close(ch)
-		fmt.Println("{{ " + title + " }}下载完成")
+		fmt.Println("{{ " + episode.title + " }}下载完成")
 		channel <- true
 	})
 	_ = cly.Visit(link)
 }
 
 func handle(baseLink string, maxConnection int64) {
-	var hrefs []string
+	var episodes []Episode
 	cly := colly.NewCollector()
 	cly.ID = 1
 	cly.OnHTML("#list", func(element *colly.HTMLElement) {
 		// 获取所有话的链接
+		decoder := mahonia.NewDecoder("gbk")
 		element.ForEach("li > a[href]", func(i int, element *colly.HTMLElement) {
 			href := element.Attr("href")
-			hrefs = append(hrefs, href)
+			title := decoder.ConvertString(element.Text)
+			episodes = append(episodes, Episode{title: title, href: href})
 		})
 
-		if maxConnection < 0 || maxConnection > int64(len(hrefs)) {
-			maxConnection = int64(len(hrefs))
+		if maxConnection < 0 || maxConnection > int64(len(episodes)) {
+			maxConnection = int64(len(episodes))
 		}
-		fmt.Println("--------- 一共" + strconv.FormatInt(int64(len(hrefs)), 10) + "话 ---------")
+		fmt.Println("--------- 一共" + strconv.FormatInt(int64(len(episodes)), 10) + "话 ---------")
 		if _, err := os.Stat("download"); os.IsNotExist(err) {
 			if err := os.Mkdir("download", 0777); err != nil {
 				fmt.Println("创建下载目录失败")
@@ -110,10 +121,10 @@ func handle(baseLink string, maxConnection int64) {
 		channel := make(chan bool, maxConnection)
 		connection := int64(0)
 		count := 0
-		for i := 0; i < len(hrefs); {
+		for i := 0; i < len(episodes); {
 			if connection < maxConnection {
 				connection++
-				go fchEachEpisode(hrefs[i], channel)
+				go fchEachEpisode(episodes[i], channel)
 				i++
 			} else {
 				<-channel
